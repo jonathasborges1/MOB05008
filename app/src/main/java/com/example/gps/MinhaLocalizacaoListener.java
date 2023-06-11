0eaee4b67a68416c54f535324f616358eb6d5149
Jonathas Borges <jbc@icomp.ufam.edu.br>
2023-06-11 00:41:21 -0400

    app gps

---
+++ app/src/main/java/com/example/gps/MinhaLocalizacaoListener.java
@@ -0,0 +1,20 @@
+package com.example.gps;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+public class MinhaLocalizacaoListener implements LocationListener {
+    public static double latitude;
+    public static double longitude;
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        location.getLatitude();
+        location.getLongitude();
+        this.latitude  = location.getLatitude();
+        this.longitude = location.getLongitude();
+    }
+}
