26a49c1da1e1774d761b8fb7e49320fc69fe3dfa
Jonathas Borges <jbc@icomp.ufam.edu.br>
2023-06-17 23:54:02 -0400

    atividade 2 - GPS entre pontos

--- GPS_Entre_Pontos/app/src/main/java/com/example/gps/MinhaLocalizacaoListener.java
+++ GPS_Entre_Pontos/app/src/main/java/com/example/gps/MinhaLocalizacaoListener.java
@@ -4,28 +4,18 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+
 public class MinhaLocalizacaoListener implements LocationListener {
     public static double latitude;
     public static double longitude;
 
     @Override
-    public void onLocationChanged(Location location) {
+    public void onLocationChanged(@NonNull Location location) {
+        location.getLatitude();
+        location.getLongitude();
         this.latitude  = location.getLatitude();
         this.longitude = location.getLongitude();
     }
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        // não precisa implementar
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-        // não precisa implementar
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-        // não precisa implementar
-    }
 }
